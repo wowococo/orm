12fe3d433705662c853d0a7432ba999f2e6bfde6
wowococo <1695599369@qq.com>
2024-02-19 16:56:53 +0800

    Initial commit

---
+++ session/raw.go
@@ -0,0 +1,39 @@
+package session
+
+import (
+	"database/sql"
+	"go-scripts/orm/dialect"
+	"go-scripts/orm/schema"
+	"strings"
+)
+
+type Session struct {
+	db       *sql.DB
+	dialect  dialect.Dialect
+	refTable *schema.Schema
+	sql      strings.Builder
+	sqlVars  []interface{}
+}
+
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
